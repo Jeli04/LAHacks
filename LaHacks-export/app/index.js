1498cf61ae0c27ded584ff82da31b1663dfc31fe
Jerry Li <56666748+Jeli04@users.noreply.github.com>
2022-04-16 11:16:46 -0700

    Switching Screens

--- LaHacks-export/app/index.js
+++ LaHacks-export/app/index.js
@@ -1,5 +1,32 @@
 import * as document from "document";
 
+let screen1 = document.getElementById("screen1");
+let screen2 = document.getElementById("screen2");
+
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+
+
+// Add more funcions for new screens 
+function showScreen1() {
+  screen1.style.display = "inline";
+  screen2.style.display = "none";
+  screen3.style.display = "none";
+}
+
+function showScreen2() {
+  screen1.style.display = "none";
+  screen2.style.display = "inline";
+  screen3.style.display = "none";  
+}
+
+
+button1.onclick = function() {
+  showScreen2();
+}
+
+
+
 let list = document.getElementById("myList");
 let items = list.getElementsByClassName("list-item");
 
@@ -16,3 +43,6 @@ let currentIndex = list.value;
 
 // Set the selected index
 list.value = 3; // Scroll to the 4th item
+
+
+
